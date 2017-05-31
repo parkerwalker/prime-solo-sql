e4f1bcc6a53c099bb1488e547550c6b2e1ea23e8
parkerwalker <parkerwalker12@gmail.com>
2017-05-31 10:40:57 -0500

    step 3

---
+++ solution.sql
@@ -0,0 +1,9 @@
+select * from syntax_practice;
+--select all users from Chicago
+select username from syntax_practice where city = 'chicago';
+--get all users with usernames that contain a
+select username from syntax_practice where username like '%a%';
+--the bank is giving new people money
+update syntax_practice set account_balance = 10 
+where transactions_completed = '0' and account_balance = '0';
+ 
