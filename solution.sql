41dce663bc8919591e7b33cd1d9c6c8f32bf6a16
parkerwalker <parkerwalker12@gmail.com>
2017-05-31 10:42:55 -0500

    step 4

--- solution.sql
+++ solution.sql
@@ -6,4 +6,6 @@ select username from syntax_practice where username like '%a%';
 --the bank is giving new people money
 update syntax_practice set account_balance = 10 
 where transactions_completed = '0' and account_balance = '0';
+--select all users that have attempted 9 or more transactions
+select username from syntax_practice where transactions_attempted > '9';
  
